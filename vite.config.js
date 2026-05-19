4de0dc4a6776e84f6f3c1329edd0daa683394698
YannGotti <1111111111aleksey46@gmail.com>
2026-05-19 23:46:53 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,47 @@ import sitemap from 'vite-plugin-sitemap'
 const API_PROXY_TARGET = 'https://api.void-rp.ru'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(),
-	sitemap({
-      		hostname: 'https://void-rp.ru',
-    	})],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://void-rp.ru',
+      dynamicRoutes: [
+        '/guide',
+        '/recipes',
+        '/market',
+        '/nations',
+        '/nations/rankings',
+        '/alliances',
+        '/leaderboard',
+        '/players/top',
+        '/shop',
+        '/download-launcher',
+        '/register',
+        '/login',
+        '/links',
+        '/privacy',
+        '/offer',
+      ],
+      exclude: [
+        '/admin',
+        '/admin/**',
+        '/internal-admin',
+        '/internal-admin/**',
+        '/profile',
+        '/profile/**',
+        '/nation/studio',
+      ],
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date(),
+    }),
+  ],
   server: {
     host: true,
     port: 5175,
     strictPort: true,
-    allowedHosts: [ "minecraftrating.ru", "127.0.0.1", "0.0.0.0"],
+    allowedHosts: ['minecraftrating.ru', '127.0.0.1', '0.0.0.0'],
     proxy: {
       '/api': {
         target: API_PROXY_TARGET,
